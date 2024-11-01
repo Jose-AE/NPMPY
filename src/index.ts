c2216e663b3a28e184056614b3b446bc389e40af
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2024-10-31 19:51:16 -0600

    Add example to exec

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 
+import { exec } from "child_process";
+
 const args = process.argv.slice(2);
-console.log("Arguments:", args);
+
+exec("python -m venv ./.venv", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Standard error: ${stderr}`);
+    return;
+  }
+  console.log(`Standard output: ${stdout}`);
+});
